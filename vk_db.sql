fa3a3ba7a04098f2ce1eff254edc98d972b2fc18
vinod-k <vinodkumark05@gmail.com>
2018-05-04 00:24:09 +0530

    sql file

---
+++ vk_db.sql
@@ -0,0 +1,19 @@
+127.0.0.1/vk_db/student/		http://localhost/phpmyadmin/tbl_sql.php?db=vk_db&table=student&token=0f643015d79eaf8429726f152a10dbf7
+ Current selection does not contain a unique column. Grid edit, checkbox, Edit, Copy and Delete features are not available. 
+
+Your SQL query has been executed successfully.
+
+desc `student`
+
+
+
+Name	varchar(255)	NO		NULL		
+City	varchar(255)	NO		NULL		
+DOB	datetime	NO		NULL		
+Street	varchar(255)	NO		NULL		
+State	varchar(255)	NO		NULL		
+Country	varchar(255)	NO		NULL		
+Pincode	int(11)	NO		NULL		
+ContactNumber	int(11)	NO		NULL		
+emailid	varchar(320)	NO		NULL		
+password	varchar(32)	NO		NULL		
